ad1373fd9fe5a2aef817fe24fdd6556fe89679f5
serifahmetharas <serifahmetharas@gmail.com>
2022-03-10 17:12:04 +0300

    Inıtial Commit
    Entity Framework-Migration,
    Models and Views for Agencies,
    CRUD operations

---
+++ Controllers/AgencyController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreAgency.Models;
+
+namespace CoreAgency.Controllers
+{
+    public class AgencyController : Controller
+    {
+        Context db = new Context();
+        public IActionResult Index()
+        {
+            var agencies = db.Agencies.ToList();
+            return View(agencies);
+        }
+
+        [HttpGet]
+        public IActionResult NewAgency()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NewAgency(Agency agency)
+        {
+            db.Agencies.Add(agency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+
+        public IActionResult DeleteAgency(int id)
+        {
+            var agency = db.Agencies.Find(id);
+            db.Agencies.Remove(agency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult GetAgency(int id) // Guncelleme içi gelmesini sağladım.
+        {
+            var agency = db.Agencies.Find(id);
+            return View("GetAgency",agency);
+        }
+
+        public IActionResult UpdateAgency(Agency agency)
+        {
+            var willupdate = db.Agencies.Find(agency.Id);
+            willupdate.Name = agency.Name;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+            
+        }
+
+
+    }
+}
---
+++ Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAgency.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; database=agencydb; integrated security=true;");
+        }
+
+        public DbSet<Agency> Agencies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
